62015e82ae0ce4f671ae6a44c9a50e56053714ef
badza <dusan.badza@yahoo.com>
2025-09-22 11:24:08 +0200

    build fix

--- apps/shops/app/layout.tsx
+++ apps/shops/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 
+import "./globals.css";
 import "@workspace/ui/globals.css";
-import "../globals.css";
 
 import { Providers } from "../components/providers";
 
